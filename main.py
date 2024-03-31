c16b61b8af7e64d278f3f21a2c57a73908d55965
Rahul Balakavi <rbalakavi@roblox.com>
2024-03-31 13:20:21 +0530

    Add pymupdf dependency and extraction

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import pdfplumber as pdfplumber
 import streamlit as st
+import fitz
 
 st.title('PDF extraction for RAG')
 
-
-def extract_data(feed):
-    data = []
-    with pdfplumber.load(feed) as pdf:
-        pages = pdf.pages
-        for p in pages:
-            data.append(p.extract_tables())
-    return data  # build more code to return a dataframe
+def extract_data(file_name):
+    doc = fitz.open(file_name)  # open a document
+    pdf_data = []
+    for page in doc:  # iterate the document pages
+        text = page.get_text().encode("utf8")  # get plain text (is in UTF-8)
+        pdf_data.append(text)
+        pdf_data.append("------------------------------")
+    return pdf_data
 
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
